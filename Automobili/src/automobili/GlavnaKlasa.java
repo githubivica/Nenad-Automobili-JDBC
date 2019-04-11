e76494348dc679a992121213980b964b34d2ae67
HP <HP@DESKTOP-J39MHQ0>
2019-04-11 20:06:05 +0200

    dodao sam metodu za unos marke

--- Automobili/src/automobili/GlavnaKlasa.java
+++ Automobili/src/automobili/GlavnaKlasa.java
@@ -4,7 +4,8 @@ public class GlavnaKlasa {
 
 	public static void main(String[] args) {
 		
-		
+		Metode metode = new Metode();
+		metode.ubaciMarku("Fiat", "Italija");
 		
 		
 	}
--- Automobili/src/automobili/Metode.java
+++ Automobili/src/automobili/Metode.java
@@ -2,6 +2,7 @@ package automobili;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Metode {
@@ -15,6 +16,46 @@ public class Metode {
 		return DriverManager.getConnection(url, userName, password);
 		
 	}
+	
+	public void ubaciMarku(String nazivMarke, String zemlja) {
+		
+		Connection konekcija = null;
+		PreparedStatement pst = null;
+		
+		try {
+			konekcija = konektujSe("automobili");
+			String query = "INSERT INTO marke VALUES (null,?,?)";
+			pst = konekcija.prepareStatement(query);
+				pst.setString(1, nazivMarke);
+				pst.setString(2, zemlja);
+			pst.execute();
+			System.out.println("Uspesan unos");
+			
+		} catch (SQLException e) {
+			System.out.println("Neuspesan unos");
+			e.printStackTrace();
+		} finally {
+			
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				konekcija.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}	
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 }
