aaf2174d8199a7577f69ad6277856cfcd832580b
HP <HP@DESKTOP-J39MHQ0>
2019-04-16 18:44:47 +0200

    dodao klasu Marke

---
+++ Automobili/src/automobili/Marke.java
@@ -0,0 +1,29 @@
+package automobili;
+
+public class Marke {
+	
+	private int idMarke;
+	private String imeMarke;
+	private String zemlja;
+	
+	public int getIdMarke() {
+		return idMarke;
+	}
+	public void setIdMarke(int idMarke) {
+		this.idMarke = idMarke;
+	}
+	public String getImeMarke() {
+		return imeMarke;
+	}
+	public void setImeMarke(String imeMarke) {
+		this.imeMarke = imeMarke;
+	}
+	public String getZemlja() {
+		return zemlja;
+	}
+	public void setZemlja(String zemlja) {
+		this.zemlja = zemlja;
+	}
+	
+	
+}
